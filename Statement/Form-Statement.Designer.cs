2295d2fdbbf38024e5285bc15d29406c26f5ef31
ArticleArtGH <56726186+ArticleArtGH@users.noreply.github.com>
2020-01-25 16:50:10 +0300

    Add files via upload
    Таблица заполняется, можно ещё за рандомом понаблюдать, хотя пока норм работает.

--- Statement/Form-Statement.Designer.cs
+++ Statement/Form-Statement.Designer.cs
@@ -28,12 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage_Statement = new System.Windows.Forms.TabPage();
             this.label_Count_Subject = new System.Windows.Forms.Label();
@@ -165,6 +165,7 @@
             this.button_AddByHand.TabIndex = 3;
             this.button_AddByHand.Text = "Добавить вручную";
             this.button_AddByHand.UseVisualStyleBackColor = true;
+            this.button_AddByHand.Click += new System.EventHandler(this.button_AddByHand_Click);
             // 
             // button_ReadFile
             // 
@@ -293,33 +294,33 @@
             // 
             // chart2
             // 
-            chartArea5.Name = "ChartArea1";
-            this.chart2.ChartAreas.Add(chartArea5);
-            legend5.Name = "Legend1";
-            this.chart2.Legends.Add(legend5);
+            chartArea1.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart2.Legends.Add(legend1);
             this.chart2.Location = new System.Drawing.Point(346, 24);
             this.chart2.Name = "chart2";
-            series5.ChartArea = "ChartArea1";
-            series5.Legend = "Legend1";
-            series5.Name = "Series1";
-            this.chart2.Series.Add(series5);
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart2.Series.Add(series1);
             this.chart2.Size = new System.Drawing.Size(300, 300);
             this.chart2.TabIndex = 1;
             this.chart2.Text = "chart2";
             // 
             // chart1
             // 
-            chartArea6.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea6);
-            legend6.Name = "Legend1";
-            this.chart1.Legends.Add(legend6);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
             this.chart1.Location = new System.Drawing.Point(6, 21);
             this.chart1.Name = "chart1";
-            series6.ChartArea = "ChartArea1";
-            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
-            series6.Legend = "Legend1";
-            series6.Name = "Series1";
-            this.chart1.Series.Add(series6);
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
             this.chart1.Size = new System.Drawing.Size(300, 300);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
--- Statement/Form-Statement.cs
+++ Statement/Form-Statement.cs
@@ -27,14 +27,60 @@ namespace Statement
         private void button_RandomGeneration_Click(object sender, EventArgs e)
         {
             //int intRow = integerRnd.Next(10, 20), intColumn = integerRnd.Next(10,20);
+            int intCell = 0, int_NA_Cell = 0,i = 0, j = 0, NA_Rows=0, NA_Columns=0; 
             int countRows = this.dataGridView_Statement.RowCount, countColumn = this.dataGridView_Statement.ColumnCount;
-            for(int i=0; i < countRows; i++ )
+            //Заполнение Зголовков
+            for (i=0; i < countRows; i++ )
             {
                 this.dataGridView_Statement.Rows[i].HeaderCell.Value = "Студент"+(i+1);
             }
-            for (int j=0; j < countColumn; j++)
+            for (j=0; j < countColumn; j++)
             {
                 this.dataGridView_Statement.Columns[j].HeaderCell.Value = "Предмет" + (j+1);
+            }
+            //Заполнение Таблицы
+            for (i = 0; i < countRows; i++)
+            {
+                for (j = 0; j < countColumn; j++)
+                {
+                    intCell = integerRnd.Next(2, 6);//Рандом делает максимальное значение на единицу ментше, т.е. для 5 от 2 до 6
+                    //this.dataGridView_Statement.Rows[j].Cells[i].Value= intCell;
+                    this.dataGridView_Statement[j, i].Value = Convert.ToString(intCell);
+                }
+                //Рандом для н/а
+                NA_Rows = integerRnd.Next(0, countRows);
+                NA_Columns = integerRnd.Next(0, countColumn);
+                //if (countRows == 1 && countColumn == 1)
+                //{
+                //    switch (integerRnd.Next(0, 2))
+                //    {
+                //        case 0: break;
+                //        case 1: this.dataGridView_Statement[NA_Columns, NA_Rows].Value = "н/а"; break;
+                //    }
+                //}
+                //else
+                //{
+                //    switch (integerRnd.Next(0, 4))
+                //    {
+                //        case 0: break;
+                //        case 1: this.dataGridView_Statement[--j, NA_Rows].Value = "н/а"; break;//Здесь j-1, т.к. j уже увеличился //инкремент сначало действие(+|-) потом инициализирует, а посткримент сначало инициализирует потом действие(+|-)
+                //        case 2: this.dataGridView_Statement[NA_Columns, i].Value = "н/а"; break;//Здесь i не вычетали 1, т.к. i ещё не увеличивался
+                //        case 3: this.dataGridView_Statement[NA_Columns, NA_Rows].Value = "н/а"; break;
+                //    }
+                //}
+                //Вот это лучше Рандомит
+                switch (integerRnd.Next(0, 2))
+                {
+                    case 0: break;
+                    case 1:
+                        switch (integerRnd.Next(1, 4))
+                        {
+                            case 1: this.dataGridView_Statement[--j, NA_Rows].Value = "н/а"; break;//Здесь j-1, т.к. j уже увеличился //инкремент сначало действие(+|-) потом инициализирует, а посткримент сначало инициализирует потом действие(+|-)
+                            case 2: this.dataGridView_Statement[NA_Columns, i].Value = "н/а"; break;//Здесь i не вычетали 1, т.к. i ещё не увеличивался
+                            case 3: this.dataGridView_Statement[NA_Columns, NA_Rows].Value = "н/а"; break;
+                        }
+                        break;
+                }
             }
             //this.dataGridView_Statement.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllHeaders;
 
@@ -76,6 +122,11 @@ namespace Statement
         private void numericUpDown_CountSubject_ValueChanged(object sender, EventArgs e)
         {
             this.dataGridView_Statement.ColumnCount = Convert.ToInt32(numericUpDown_CountSubject.Value);
+        }
+
+        private void button_AddByHand_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
